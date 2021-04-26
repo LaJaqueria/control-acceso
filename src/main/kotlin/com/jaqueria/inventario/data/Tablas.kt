1da0dd7bf028eed6ba1a8d4ff51b79330bbed31b
zerasul <zerasul@gmail.com>
2021-04-26 16:38:36 +0200

    corrigiendo clases de datos

--- src/main/kotlin/com/jaqueria/inventario/data/Tablas.kt
+++ src/main/kotlin/com/jaqueria/inventario/data/Tablas.kt
@@ -1,43 +1,31 @@
 package com.jaqueria.inventario.data
 
-import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
+import javax.persistence.*
 
 class Tablas {
     @Entity
     @Table(name = "dispositivos")
-    class Dispositivos {
-        @Id
-        var id: Int = 0
-        var num_serie: Int = 0 //KEY ÚNICA
-        var uid_socio: Int = 0
-        var activo: Boolean = false
+         class Dispositivos(
+             @Id
+             var id: Int = 0,
+             @Column(unique = true)
+             var num_serie: Int = 0,
+             var uid_socio: Int = 0,
+             var activo: Boolean = false,
+             @OneToMany
+             var registros: MutableList<Registros> = ArrayList()
+            )
 
-        //var uid: String = ""
-        constructor(id: Int, num_serie: Int, uid_socio: Int, activo: Boolean) {
-            this.id = id
-            this.num_serie = num_serie
-            this.uid_socio = uid_socio
-            this.activo = activo
-        }
-    }
+         //Registros
+         @Entity
+         @Table(name = "registros")
+         class Registros (
+             @Id
+             var id: Int = 0,
+             @JoinColumn(name = "id")
+             var id_tarjeta: Tablas.Dispositivos,
+             var fecha_hora: String = "",
+             var descripcion: String = ""
+            )
 
-    //Registros
-    @Entity
-    @Table(name = "registros")
-    class Registros {
-        @Id
-        var id: Int = 0
-        var id_tarjeta: Int = 0 //FOREIGN KEY
-        var fecha_hora: String = ""
-        var descripcion: String = ""
-
-        constructor(id: Int, id_tarjeta: Int, fecha_hora: String, descripcion: String) {
-            this.id = id
-            this.id_tarjeta = id_tarjeta
-            this.fecha_hora = fecha_hora
-            this.descripcion = descripcion
-        }
-    }
 }
