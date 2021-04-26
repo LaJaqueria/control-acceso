872006829f7c722ece57de3bb4f93a590bf95324
aloaisa <aloaisa@hotmail.com>
2021-04-26 15:25:41 +0200

    Se añade el archivo intermedio Service, para no llamar directamente a los repositorios y llamarlos a por medio del servicio.
    Se añade licencia gpl3
    
    Se añade captura y modificación de pom.xml por problemas de desconfiguración en versiones maven

--- src/main/kotlin/com/jaqueria/inventario/controller/TablesController.kt
+++ src/main/kotlin/com/jaqueria/inventario/controller/TablesController.kt
@@ -3,30 +3,42 @@ package com.jaqueria.inventario.controller
 import com.jaqueria.inventario.data.DispositivosRepository
 import com.jaqueria.inventario.data.RegistrosRepository
 import com.jaqueria.inventario.data.Tablas
+import com.jaqueria.inventario.data.Tablas.*
+import com.jaqueria.inventario.service.TablaService
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class TablasController(
-    private val dispositivosRepository: DispositivosRepository,
-    private val registrosRepository: RegistrosRepository
+    private val tablaService: TablaService
 ) {
+
+
+    /**private val dispositivosRepository: DispositivosRepository,
+    private val registrosRepository: RegistrosRepository
+    ) {
     @GetMapping("/dispositivos")
     fun listDispositivos(@RequestParam type: String?): List<Tablas.Dispositivos> {
-        return dispositivosRepository.findAll();
+    return dispositivosRepository.findAll();
     }
 
     @PostMapping("/dispositivos")
     fun createDispositivos(@RequestBody dispositivos: Tablas.Dispositivos) {
-        dispositivosRepository.save(dispositivos);
+    dispositivosRepository.save(dispositivos);
     }
 
     @GetMapping("/registros")
     fun listRegistros(@RequestParam type: String?): List<Tablas.Registros> {
-        return registrosRepository.findAll();
+    return registrosRepository.findAll();
     }
 
     @PostMapping("/registros")
     fun createRegistros(@RequestBody registros: Tablas.Registros) {
-        registrosRepository.save(registros);
+    registrosRepository.save(registros);
+    }*/
+    fun listDispositivos(@RequestParam type: String?): List<Tablas.Dispositivos> {
+        return tablaService.getAllDevices();
     }
 }
+
+
+
--- src/main/kotlin/com/jaqueria/inventario/service/TablaService.kt
+++ src/main/kotlin/com/jaqueria/inventario/service/TablaService.kt
@@ -5,7 +5,10 @@ import com.jaqueria.inventario.data.RegistrosRepository
 import com.jaqueria.inventario.data.Tablas
 
 class TablaService(private val dispositivosRepository: DispositivosRepository, private val registrosRepository: RegistrosRepository) {
-    public fun getDevices(id: Int):Tablas.Dispositivos{
+    /*public fun getDevices(id: Int):Tablas.Dispositivos{
         return dispositivosRepository.findById(id).get()
+    }*/
+    public fun getAllDevices():List<Tablas.Dispositivos>{
+        return dispositivosRepository.findAll()
     }
 }
