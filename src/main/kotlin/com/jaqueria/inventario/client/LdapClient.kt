60ded8bac524302fc93af49260649d701cf9380b
zerasul <zerasul@gmail.com>
2021-05-07 21:27:44 +0200

    creando cliente para LDAP

--- src/main/kotlin/com/jaqueria/inventario/client/LdapClient.kt
+++ src/main/kotlin/com/jaqueria/inventario/client/LdapClient.kt
@@ -4,56 +4,91 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.ldap.core.AttributesMapper
 import org.springframework.ldap.core.LdapTemplate
 import org.springframework.ldap.core.support.LdapContextSource
+import org.springframework.ldap.query.LdapQuery
+import org.springframework.ldap.query.LdapQueryBuilder
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
+import javax.naming.directory.Attributes
 
-
+/**
+ * LDap Client
+ */
 @Component
 class LdapClient {
 
-    @Value("\${ldap.url}")
-    private var ldapUrl: String? = null;
-
+    /**
+     * LdapContextSource
+     */
     private var ldapcontextSource: LdapContextSource? = LdapContextSource();
 
+    /**
+     * LDAP Urls
+     */
     @Value("\${ldap.urls}")
     private lateinit var baseUrl: String
 
+    /**
+     * Ldap Base DN
+     */
     @Value("\${ ldap.base.dn}")
     private lateinit var base: String
 
+    /**
+     * Ldap Username
+     */
     @Value("\${ldap.username}")
     private lateinit var username: String
 
+    /**
+     * Ldap PAssword
+     */
     @Value("\${ldap.password}")
     private lateinit var ldapPassword: String
 
 
+    /**
+     * Inicializacion de LDAP
+     */
     @PostConstruct
     fun initContext() {
-        ldapcontextSource!!.setUrl(ldapUrl);
+        this.ldapcontextSource?.setUrl(baseUrl)
+        this.ldapcontextSource?.setBase(base)
+        this.ldapcontextSource?.userDn = (username)
+        this.ldapcontextSource?.password = (ldapPassword)
     }
 
-    private fun contextSource(): LdapContextSource? {
-        val contextSource = LdapContextSource()
 
-        contextSource.setUrl(baseUrl)
-        contextSource.setBase(base)
-        contextSource.userDn = (username)
-        contextSource.password = (ldapPassword)
-
-        return contextSource
-    }
-
+    /**
+     * OBtener el cliente para LDAP
+     */
     private fun getClient():LdapTemplate    {
         return LdapTemplate(this.ldapcontextSource)
     }
 
-    fun ldapSearch(uid: String): List<String> {
-        var lt = getClient ()
-        lt.search("cn=${uid}",   attrs -> (String) attrs.get("cn").get()); )
+    /**
+     * Consulta LDAP para buscar un usuario
+     */
+    fun ldapSearch(uid: String): List<UserLDAP> {
+        val lt = getClient ()
+        return lt.search("","cn=${uid}",   UsuarioMapper())
 
-        return emptyList()
+    }
+
+    /**
+     * Clase para guardar la informacion de un usuario LDAP
+     */
+    data class UserLDAP(val cn:String,val firstname:String,val secondName:String);
+
+    /**
+     * Usuario MApper
+     */
+    class UsuarioMapper: AttributesMapper<UserLDAP> {
+
+        override fun mapFromAttributes(attrs: Attributes?): UserLDAP {
+                return UserLDAP(attrs?.get("cn") as String,attrs.get("sn") as String,attrs.get("sn") as String)
+        }
+    }
+}
 
 
 
